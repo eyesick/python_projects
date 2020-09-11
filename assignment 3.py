4a1d7bfdfda37c1def245fc3bf88560d10b4c248
eyesick <59859373+eyesick@users.noreply.github.com>
2020-09-11 13:00:26 -0600

    Update assignment 3.py

--- assignment 3.py
+++ assignment 3.py
@@ -2,10 +2,22 @@ class phone:
     os = ""
     cpu = ""
     ram = ""
+
+    def phoneop(self):
+        msg = "\n i am a basic phone i can make and recieve calls and text"
+        print(msg)
 class iphone(phone):
     model= ""
     iosver = ""
+
+    def phoneop(self):
+        msg = "\n i am a apple smart phone i can make and recieve calls and text as well as use the apple app store and apple services such as face time and i message"
+        print(msg)
+        
 class android(phone):
     model = ""
     andriodver = ""
     
+    def phoneop(self):
+        msg = "\n i am a android phone i can make and recieve calls and text as well as use the google play store and other andriod services"
+        print(msg)
